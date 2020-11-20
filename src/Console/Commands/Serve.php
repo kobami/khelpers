1a5d065172872e5f2021a6c409412d51f00a67f6
Vlad <vlad@kobami.com>
2020-11-20 07:27:53 -0600

    adds command to serve local site

---
+++ src/Console/Commands/Serve.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Kobami\Khelpers\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+
+class Serve extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'khelpers:serve';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Serve site at localhost:8000';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $url = env('APP_URL');
+        $host = parse_url($url, PHP_URL_HOST);
+        $port = parse_url($url, PHP_URL_PORT);
+
+        $this->call('serve', ['--host' => $host, '--port' => $port]);
+    }
+}
